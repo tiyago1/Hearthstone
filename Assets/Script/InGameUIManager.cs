3ada9c144932e8903320bb7bb2339ee9edcdc2ef
CASPERNIRVANA\AliEnver <tiyago1@gmail.com>
2016-11-18 15:32:51 +0300

    prefabs added

--- Assets/Script/InGameUIManager.cs
+++ Assets/Script/InGameUIManager.cs
@@ -30,7 +30,16 @@ public class InGameUIManager : GuiPanel
         PlayerCardDeck.Initialize(this);
         CardConfirmationPanel.Initialize(this);
         ConfirmCardPanel.Initialize(this);
+        PlayerCardDeck.Initialize(this);
+        CardConfirmationPanel.OnCardConfimated += CardConfirmationPanel_OnCardConfimated;
 	}
+
+    void CardConfirmationPanel_OnCardConfimated(int index)
+    {
+        Debug.Log("Confirmated Play");
+        PlayerCardDeck.RemoveAtCardDeck(index);
+        
+    }
 
 	#endregion // Public Methods
 }
